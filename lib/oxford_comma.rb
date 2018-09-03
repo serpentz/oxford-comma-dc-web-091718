9b1db1fa4979ee3820570626421be96c3a8e8d22
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 14:44:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,10 @@ if array.size == 2
   return "#{array[0]} and #{array[1]}"
 elsif array.size > 2
 array.insert(array.length-1,"and ")
+str = array.last(2).join
+array.delete_at(array.length)
+array.delete_at(array.length)
+array.push(str)
  end
  array.join(", ")
 end
