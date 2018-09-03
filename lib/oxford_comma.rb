81f94a6667462967979520026eb90958945dc33e
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 14:32:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,10 @@
 def oxford_comma(array)
-if array.size > 2
-  array.join(", ")
- else array
+if array.size == 2
+  "#{array[0]} and #{array[1]}"
+elsif array>2
+array[array.length].insert(0,"and")
+array.join(", ")
+else
+  array
  end
 end
