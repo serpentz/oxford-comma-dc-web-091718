84153996ec6b4671df7b4e92e52b905fd3e6094a
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 14:39:03 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
 if array.size == 2
   return "#{array[0]} and #{array[1]}"
 elsif array.size > 2
-array.insert(array.length,"and ")
+array.insert(array.length-1,"and ")
  end
  array.join(", ")
 end
