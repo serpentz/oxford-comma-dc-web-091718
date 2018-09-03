c738ff56cb3592695d45d0b40814399880e008c6
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 14:52:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def oxford_comma(array)
 if array.size == 2
   return "#{array[0]} and #{array[1]}"
@@ -7,6 +8,9 @@ str = array.last(2).join
 array.delete_at(array.length)
 array.delete_at(array.length)
 array.push(str)
+binding.pry
  end
  array.join(", ")
 end
+
+oxford_comma(["kiwi", "durian", "starfruit"])
