7bdeb4b1a056102f109176852fc466e882a4966a
Absalom Cherinet <absalomcherinet@gmail.com>
2018-09-03 14:57:48 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
+require 'pry'
 def oxford_comma(array)
+if array.size == 2
+  return "#{array[0]} and #{array[1]}"
+elsif array.size > 2
+array.insert(array.length-1,"and ")
+str = array.last(2).join
+array.pop(2)
+array.push(str)
 
+ end
+ array.join(", ")
 end
